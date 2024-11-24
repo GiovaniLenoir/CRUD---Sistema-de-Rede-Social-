b422316d49c08d8a5e50946cd2b78055831e301c
giovaniLenoir <gionanilenoir@gmail.com>
2024-11-24 16:40:13 -0300

    Classe Comentario

---
+++ Comentario.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Comentario {
+    private Integer id;
+    private Usuario autor;
+    private String conteudo;
+    private LocalDateTime dataComentario;
+    private List<Post> Post;
+    
+}
